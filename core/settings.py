975891a20b93909f77dd44c218eec23fef245537
cap-ma <uone2323@gmail.com>
2024-03-09 17:19:32 +0500

    fix

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
+    
 
     'drf_yasg',
     'rest_framework',
