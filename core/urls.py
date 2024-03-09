907357d45d9620cc631f77f1bd725fa3d3f16adb
cap-ma <uone2323@gmail.com>
2024-03-09 17:05:10 +0500

    media files

--- core/urls.py
+++ core/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework import permissions
@@ -45,3 +48,5 @@ urlpatterns = i18n_patterns(
     prefix_default_language=False
 )
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
